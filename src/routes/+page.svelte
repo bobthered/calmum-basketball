b08041636f1544c662c3ec1d7a4cdb25f7c4e626
Bob McAleavey <bobmcaleavey@gmail.com>
2025-12-01 07:18:47 -0500

    fix: dateString needs to be padded

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -83,7 +83,7 @@
 	);
 	const date = $derived.by(() => new Date());
 	const dateString = $derived.by(() => {
-		const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+		const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
 		return formattedDate;
 	});
 	const isAnswered = $derived.by(() => answer !== undefined);
